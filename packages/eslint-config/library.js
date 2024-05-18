1509cd81d56ce85b3f8660241a662ff7814798ac
chidubemovbiebo <chidubemovbiebo@gmail.com>
2024-05-19 00:31:22 +0100

    Setup

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -4,11 +4,14 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "eslint-config-turbo"],
   plugins: ["only-warn"],
   globals: {
     React: true,
     JSX: true,
+  },
+  parserOptions: {
+    project: true,
   },
   env: {
     node: true,
@@ -31,4 +34,12 @@ module.exports = {
       files: ["*.js?(x)", "*.ts?(x)"],
     },
   ],
+  rules: {
+    "new-cap": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+  },
 };
